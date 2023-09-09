9345be98c4bc5d58bf941930ae4ddd759b3eccb9
usmanmq00 <usmanmq00@gmail.com>
2023-09-09 13:41:17 +0500

    Database updated

---
+++ client/build/precache-manifest.88fb2803287b033af374db68fcd8da8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fda5f74e8592fee545db88495707709",
+    "url": "/devcom/index.html"
+  },
+  {
+    "revision": "7cff62f55aad4d7e474e",
+    "url": "/devcom/static/css/main.b813fd81.chunk.css"
+  },
+  {
+    "revision": "f1fc8954728d39995952",
+    "url": "/devcom/static/js/2.e41564ed.chunk.js"
+  },
+  {
+    "revision": "65af9bd3d7d331953a14241a01ce8643",
+    "url": "/devcom/static/js/2.e41564ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cff62f55aad4d7e474e",
+    "url": "/devcom/static/js/main.c4462a67.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/devcom/static/js/main.c4462a67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4050620d9a678e44b88c",
+    "url": "/devcom/static/js/runtime-main.95af7075.js"
+  }
+]);
